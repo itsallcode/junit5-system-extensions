282c0bd1d39dcd4ab3fbe9e70f9fe0623eac4556
Sebastian Bär <redcatbear@ursus-minor.de>
2021-05-02 11:46:10 +0200

    #27: Fixed SONAR code smells. (#29)
    * #27: Fixed SONAR code smells.
    
    * Apply suggestions from code review
    
    Co-authored-by: chπ <kaklakariada@chp1.net>
    
    * #27: Fix mixup of System.out/err in comment
    
    Co-authored-by: chπ <kaklakariada@chp1.net>
    Co-authored-by: kaklakariada <kaklakariada@users.noreply.github.com>

---
+++ doc/changes/changes_1.2.1.md
@@ -0,0 +1,11 @@
+# JUnit5 System Extensions 1.2.1, released 2021-05-02
+
+Code name: Sonar smell fixes for 1.2.0
+
+## Summary
+
+Version 1.2.1 is a service release that fixed SONAR code smells.
+
+## Bugfixes
+
+* [#27](https://github.com/itsallcode/junit5-system-extensions/issues/27): Fixed SONAR code smells of version 1.2.0.
--- src/test/java/org/itsallcode/junit/sysextensions/TestSystemErr.java
+++ src/test/java/org/itsallcode/junit/sysextensions/TestSystemErr.java
@@ -9,13 +9,13 @@ package org.itsallcode.junit.sysextensions;
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * This Source Code may also be made available under the following Secondary
  * Licenses when the conditions for such availability set forth in the Eclipse
  * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
  * with the GNU Classpath Exception which is
  * available at https://www.gnu.org/software/classpath/license.html.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  * #L%
  */
@@ -33,7 +33,7 @@ class TestSystemErr
     void testCapture(final Capturable stream)
     {
         stream.capture();
-        assertEquals(stream.getCapturedData(), "");
+        assertEquals("", stream.getCapturedData());
     }
 
     @Test
@@ -44,7 +44,7 @@ class TestSystemErr
         stream.capture();
         final String expected = "This text must be captured." + System.lineSeparator();
         System.err.print(expected);
-        assertEquals(stream.getCapturedData(), expected);
+        assertEquals(expected, stream.getCapturedData());
     }
 
     @Test
@@ -55,6 +55,6 @@ class TestSystemErr
         stream.capture();
         final String expected = "Again: this must be captured." + System.lineSeparator();
         System.err.print(expected);
-        assertEquals(stream.getCapturedData(), expected);
+        assertEquals(expected, stream.getCapturedData());
     }
 }

68c23dd14c4b3241bdf72569cc0b8fbcaaf0070e
kaklakariada <kaklakariada@users.noreply.github.com>
2024-07-07 14:51:22 +0200

    Fix typo

--- src/test/java/org/itsallcode/io/TestCapturingOutputStream.java
+++ src/test/java/org/itsallcode/io/TestCapturingOutputStream.java
@@ -1,19 +1,9 @@
 package org.itsallcode.io;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.*;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-class TestCapturingOuputStream
+import java.io.*;ream
 {
     private static final byte[] CONTENT = "content".getBytes();
     private OutputStream delegate;
